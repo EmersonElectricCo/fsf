34223dda3363bb2ca3f3a755270b0c8f77a6a853
Jason <jxbatchelor@gmail.com>
2016-02-09 15:09:13 -0600

    Update config.py

--- fsf-client/conf/config.py
+++ fsf-client/conf/config.py
@@ -10,4 +10,4 @@ SERVER_CONFIG = { 'IP_ADDRESS' : ['127.0.0.1',],
                   'PORT' : 5800 }
 
 # Full path to debug file if run in --not-interactive mode
-CLIENT_CONFIG = { 'LOG_FILE' : '/var/log/scanner/client_dbg.log' }
+CLIENT_CONFIG = { 'LOG_FILE' : '/tmp/client_dbg.log' }
